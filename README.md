1b02846b94a0484f008d74e91bc2ddffeedac7ab
rajabinks <79781370+rajabinks@users.noreply.github.com>
2022-04-30 16:43:13 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Archive of Stooq Commodity Prices
+
+[![License](https://img.shields.io/badge/License-MIT-green)](https://github.com/rajabinks/stooq-commodities/blob/master/LICENSE)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![Imports: isort](https://img.shields.io/badge/%20imports-isort-%231674b1?style=flat&labelColor=ef8336)](https://pycqa.github.io/isort/)
+
+Due to the [issue](https://github.com/pydata/pandas-datareader/issues/925) regarding the downloading of commodity prices from [Stooq](https://stooq.com/) using [pandas-datareader](https://pandas-datareader.readthedocs.io/en/latest/index.html), this repository stores a limited archive of previously obtained data.
+
+The generation of historical data is done using `gen_data.py` with customisation options regarding asset selection and dates available within the file. Tests for all user inputs have also been written.
+
+We provide seven sets of databases outlined in the table below. Note the count refers to cleaned data which only includes days where prices are available for all included assets.
+
+These include the equity indices S\&P500 (SPX), Nasdaq 100 (NDX), Dow Jones Industrial Average (DJIA), along components of the DJIA. For commodities, we provide front month futures contract prices for gold, silver, high grade copper, platinum, palladium, WTI crude oil, RBOB gasoline, lumber, live cattle, coffee, and orange juice. The included 25 of total 30 DJIA components in DJI are not exactly the 26 in Full due to additions and removals to the index.
+
+|  | Components | Count | Start Date |
+| ----------- | ----------- | ----------- | ----------- |
+| **Equity Indices**
+| SNP | SPX | 9,167 |1985-10-01 |
+| USEI | SPX + NDX, DJIA | 9,167 | 1985-10-01 |
+| DJI | USEI + 25 DJIA Components | 8,024 | 1990-03-26 |
+| **Broader Markets**
+| Minor | USEI + Gold, Silver, WTI | 9,090 | 1985-10-01 |
+| Medium | Minor + Cooper, Platinum, Lumber | 9,077 | 1985-10-01 |
+| Major | Medium + Palladium, RBOB, Cattle, Coffee, OJ | 8,990 | 1985-10-01 |
+| Full | Major + 26 DJIA Components |7,858 | 1990-03-26 |
+
+Overall, these consist of daily time series of prices spanning from the earlier of either the first available date or the 10th October 1985. The closing dates are 10th February 2022 for Equity Indices and 24th December 2021 for Broader Markets. Equity indices can be updated whereas Broader Markets are not updatable as explained in the [issue](https://github.com/pydata/pandas-datareader/issues/925).
+
+Initialising the code involves the following commands:
+```commandline
+git clone https://github.com/rajabinks/stooq-commodities.git
+
+cd stooq-commodities
+```
+Install all required packages with or without dependencies using:
+```commandline
+pip3 install -r requirements.txt
+
+pip3 install -r requirements--no-deps.txt
+```
+Historical financial market data is sourced and aggregated using:
+```commandline
+python gen_data.py
+```
+The `.pkl` dataframes and `.csv` files are equivalent, while the `.npy` arrays are cleaned such that only dates where all assets have prices are included. The `.npy` files are also always ordered where earlier dates are indexed first, whereas the ordering in `.pkl` and `.csv` files is dependent on the communication between the Stooq API and relevant packages.
---
+++ gen_data.py
@@ -0,0 +1,159 @@
+"""
+title:                          gen_data.py
+usage:                          python gen_data.py
+python version:                 3.10
+pandas-datareader version:      0.10
+
+code style:                     black==22.3
+import style:                   isort==5.10
+
+Description:
+    Collects market data from online sources and creates NumPy arrays ready for
+    agent learning. All data is remotely (and freely) obtained using pandas-datareader
+    following https://pydata.github.io/pandas-datareader/remote_data.html.
+
+    Historical data for major indices, commodities, and currencies is obtained from
+    Stooq at https://stooq.com/. Note not every symbol can be utilised, all must be
+    individually checked to determine feasibility.
+
+    Occasionally will receive "SymbolWarning: Failed to read symbol" from Stooq API,
+    running the script again usually fixes this but might not probably.
+
+Instructions:
+    1. Select appropriate start and end date for data for all assets with daily data
+       sampling frequency.
+    2. Enter into the dictionary the obtained Stooq symbols for desired assets and
+       place them in lisst following the naming scheme.
+    3. Running file will scrape data and place it in a directory containing pickle
+       and csv files, along with cleaned NumPy arrays.
+
+Stooq - Symbols and Data Availability:
+    ^SPX: S&P 500                       https://stooq.com/q/d/?s=^spx
+    ^DJI: Dow Jones Industrial 30       https://stooq.com/q/d/?s=^dji
+    ^NDX: Nasdaq 100                    https://stooq.com/q/d/?s=^ndx
+
+    GC.F: Gold - COMEX                  https://stooq.com/q/d/?s=gc.f
+    SI.F: Silver - COMEX                https://stooq.com/q/d/?s=si.f
+    HG.F: High Grade Copper - COMEX     https://stooq.com/q/d/?s=hg.f
+    PL.F: Platinum - NYMEX              https://stooq.com/q/d/?s=pf.f
+    PA.F: Palladium - NYMEX             https://stooq.com/q/d/?s=pa.f
+
+    CL.F: Crude Oil WTI - NYMEX         https://stooq.com/q/d/?s=cl.f
+    RB.F: Gasoline RBOB - NYMEX         https://stooq.com/q/d/?s=rb.f
+
+    LS.F: Lumber - CME                  https://stooq.com/q/d/?s=ls.f
+    LE.F: Live Cattle - CME             https://stooq.com/q/d/?s=le.f
+    KC.F: Coffee - ICE                  https://stooq.com/q/d/?s=kc.f
+    OJ.F: Orange Juice - ICE            https://stooq.com/q/d/?s=oj.f
+
+    ^ = index value
+    .C = cash
+    .F = front month futures
+"""
+
+import os
+
+import numpy as np
+import pandas as pd
+import pandas_datareader.data as pdr
+
+from utils import dataframe_to_array, market_data_tests
+
+"""
+    ********************************************************************************
+    AS OF THE TIMING OF THIS MOST RECENT COMMIT, STOOQ NO LONGER ALLOWS THE
+    DOWNLOADING OF COMMODITIES DATA AND SO THEY WILL RETURN AN ERROR.
+    https://github.com/pydata/pandas-datareader/issues/925
+    https://github.com/rajabinks/stooq-commodities
+    ********************************************************************************
+"""
+
+# common starting/endiing dates for daily data collection for all assets
+start: str = "1985-10-01"
+end: str = "2022-02-10"
+
+# fmt: off
+
+stooq: dict = {
+    # pairs for data saving and assets to be included
+    # market_id: [market_name, included assets (List[str])]
+
+    "mkt0": ["snp", ["^SPX"]],
+
+    "mkt1": ["usei", ["^SPX", "^DJI", "^NDX"]],
+
+    "mkt2": ["dji", ["^SPX", "^DJI", "^NDX",
+                     "AAPL.US", "AMGN.US", "AXP.US", "BA.US", "CAT.US", "CVX.US",
+                     "DIS.US", "HD.US", "IBM.US", "INTC.US", "JNJ.US", "JPM.US",
+                     "KO.US", "MCD.US", "MMM.US", "MRK.US", "MSFT.US", "NKE.US",
+                     "PFE.US", "PG.US", "VZ.US", "WBA.US", "WMT.US",
+                     "CSCO.US", "UNH.US",                              # starts 1990
+                    #   "CRM.US", "DOW.US", "GS.US", "TRV.US", "V.US"    # very little data
+                    ]],
+
+    # unable to update commodities data from Stooq
+
+    # "mkt3": ["minor", ["^SPX", "^DJI", "^NDX",
+    #                    "GC.F", "SI.F",
+    #                    "CL.F"
+    #                    ]],
+
+    # "mkt4": ["medium", ["^SPX", "^DJI", "^NDX",
+    #                     "GC.F", "SI.F", "HG.F", "PL.F",
+    #                     "CL.F",
+    #                     "LS.F"
+    #                     ]],
+
+    # "mkt5": ["major", ["^SPX", "^DJI", "^NDX",
+    #                    "GC.F", "SI.F", "HG.F", "PL.F", "PA.F",
+    #                    "CL.F", "RB.F",
+    #                    "LS.F", "LE.F", "KC.F", "OJ.F"
+    #                    ]],
+
+    # "mkt6": ["full", ["^SPX", "^DJI", "^NDX",
+    #                   "GC.F", "SI.F", "HG.F", "PL.F", "PA.F",
+    #                   "CL.F", "RB.F",
+    #                   "LS.F", "LE.F", "KC.F", "OJ.F",
+    #                   "AAPL.US", "AXP.US", "BA.US", "CAT.US", "CVX.US",
+    #                   "DIS.US", "HD.US", "IBM.US", "INTC.US", "JNJ.US", "JPM.US",
+    #                   "KO.US", "MCD.US", "MMM.US", "MRK.US", "MSFT.US", "NKE.US",
+    #                   "PFE.US", "PG.US", "RTX.US", "VZ.US", "WBA.US", "WMT.US", "XOM.US"
+    #                   "CSCO.US", "UNH.US",                   # starts 1990
+    #                #    "DOW.US", "GS.US", "TRV.US", "V.US"    # very little data
+    #                 ]],
+    }
+
+# fmt: on
+
+if __name__ == "__main__":
+
+    # directory for saving market prices dataframes, csvs, and arrays
+    path = "./market_data/"
+    # market price type (Open, High, Low, or Close)
+    price_type = "Close"
+
+    # CONDUCT TESTS
+    market_data_tests(start, end, stooq, path, price_type)
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    for x in stooq:
+        name = "stooq_" + str(stooq[str(x)][0])
+        assets = stooq[str(x)][1]
+
+        scraped_data = pdr.get_data_stooq(assets, start, end)
+        scraped_data.to_pickle(path + name + ".pkl")
+
+        market = pd.read_pickle(path + name + ".pkl")
+
+        market.to_csv(path + name + ".csv")
+
+        prices = dataframe_to_array(market, price_type)
+        np.save(path + name + ".npy", prices)
+
+        print(
+            "{}: n_assets = {}, days = {}".format(
+                name, prices.shape[1], prices.shape[0]
+            )
+        )
